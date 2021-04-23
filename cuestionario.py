38b79a3daac2088e9d10c1d61beb0f4b6bb427ce
J10pro <83092508+J10pro@users.noreply.github.com>
2021-04-23 14:52:58 -0500

    Add files via upload

---
+++ cuestionario.py
@@ -0,0 +1,27 @@
+#Por favor no robe este codigo (solo editelo y mande los respectivos creditos a J10)
+#Trato de ser lo mas limpio y natural en este tipo de codigo de programacion tan impresionante
+#https://www.youtube.com/channel/UCTxqBfQyNCj0o8g7icj1JBA
+ 
+a = "(Pregunta 1)"
+b = "(Pregunta 2)"
+c = "(Pregunta 3)"
+while True:
+	print(f"(Pregunta)")
+	print(f"(Adiscional a la pregunta si es muy larga\n\
+a. (respuesta 1)\n\
+b. (respuesta 2)\n\
+c. (respuesta 3)")
+	opcion = input()
+	if opcion == 'a':
+		print("(Respuesta)")
+		break
+
+	elif opcion == 'b':
+		print("(Respuesta)")
+		break
+
+	elif opcion == 'c':
+		print("Respuesta)")
+		break
+
+input("Presione Enter para salir de esta pantalla...")
